448efb923ee142df0c998b2c4c88b3279a837518
ejim favour <favourejim56@gmail.com>
2024-10-06 20:06:08 +0100

    implemented global interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -38,7 +39,12 @@ async function bootstrap() {
   // setup
   SwaggerModule.setup('api', app, document);
 
+  // enable cors
   app.enableCors();
+
+  // global interceptors
+  // app.useGlobalInterceptors(new DataResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
