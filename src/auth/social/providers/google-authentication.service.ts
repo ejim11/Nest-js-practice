d0a2db165aa402addbd2819e7bc96887a16b1e06
ejim favour <favourejim56@gmail.com>
2024-10-06 10:33:50 +0100

    implement auth with google token

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,8 +1,10 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { OAuth2Client } from 'google-auth-library';
 import jwtConfig from 'src/auth/config/jwt.config';
 import { GoogleTokenDto } from '../dtos/google-token.dto';
+import { UsersService } from 'src/users/providers/users.service';
+import { GenerateTokensProvider } from 'src/auth/providers/generate-tokens.provider';
 
 /**
  * if you want to instantiate some properties in the class, onModuleInit can be used
@@ -18,6 +20,17 @@ export class GoogleAuthenticationService implements OnModuleInit {
      */
     @Inject(jwtConfig.KEY)
     private readonly jwtConfiguation: ConfigType<typeof jwtConfig>,
+
+    /**
+     * injecting users service
+     */
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+
+    /**
+     * injecting generate tokens provider
+     */
+    private readonly generateTokensProvider: GenerateTokensProvider,
   ) {}
 
   onModuleInit() {
@@ -28,9 +41,21 @@ export class GoogleAuthenticationService implements OnModuleInit {
 
   public async authentication(googleTokenDto: GoogleTokenDto) {
     // verify the goole token sent by user
+    const loginTicket = await this.oauthClient.verifyIdToken({
+      idToken: googleTokenDto.token,
+    });
+
     // Extract the payload from Google Jwt
+    const { email, sub: googleId } = loginTicket.getPayload();
+
     // Find the user to the database using the GoogleId
+    const user = await this.usersService.findOneByGoogleId(googleId);
+
     // If googleId toekn exists, generate token
+    if (user) {
+      return this.generateTokensProvider.generateTokens(user);
+    }
+
     // if not create a new user and then generate tokens
     // throw unauthenticated error exception
   }
