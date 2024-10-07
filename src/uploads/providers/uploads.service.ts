f812f25ac7105e2a628f913e899e62b89a14cf33
ejim favour <favourejim56@gmail.com>
2024-10-07 09:40:47 +0100

    created the service and provider for the upload s

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  public async uploadFile(file: Express.Multer.File) {
+    // upload the fiile to the aws s3 bucket and a url has been generated
+    // Generate a new entry in the db (will contain the cloudfront url)
+  }
+}
